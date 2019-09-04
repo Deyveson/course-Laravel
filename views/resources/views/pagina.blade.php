a81f6ffac45ed1473b479ba7069bff9b41ff3cd8
Deyveson Willian <deyvesonn@outlook.com>
2019-09-04 16:55:45 -0400

    Views - Desenvolvendo componente

--- views/resources/views/pagina.blade.php
+++ views/resources/views/pagina.blade.php
@@ -1,6 +1,5 @@
 <html>
     <head>
-
         {{--    importando normal    --}}
         {{--<link href="{{ asset('css/app.css')}}" rel="stylesheet">--}}
 
@@ -8,9 +7,16 @@
         <link href="{{ URL::to('css/app.css')}}" rel="stylesheet">
     </head>
     <body>
-        <div class="alert alert-primary" role="alert">
-            Este é um alerta primário — confira!
+        <div class="container text-center">
+            <h3 style="font-size: 30px; font-weight: bold">Componente</h3>
         </div>
+
+        @component('components.meucomponente')
+            <strong>Erro: </strong> Sua mensagem de erro.
+        @endcomponent
+        {{-- Passando para o componente atraves do strong --}}
+
+
         {{--<script src="{{ asset('js/app.js')}}" type="text/javascript"></script>--}}
         <script src="{{ URL::to('js/app.js')}}" type="text/javascript"></script>
     </body>
