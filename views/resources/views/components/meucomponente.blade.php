012e6f8f58e686688cc764ce7f7c474f11644e67
Deyveson Willian <deyvesonn@outlook.com>
2019-10-04 14:13:53 -0400

    Views - Passando parâmetros pro componente (duas Formas de fazer)

--- views/resources/views/components/meucomponente.blade.php
+++ views/resources/views/components/meucomponente.blade.php
@@ -1,14 +1,18 @@
-<div class="alert alert-primary container" role="alert">
-   {{$slot}}
-{{--  recebendo do component pai 'pagina'
-tudo que e declarado no strong e recebido como '$slot' --}}
+<div class="alert alert-{{ $tipo }} container" role="alert">
+
+    <div class="alert-title"> {{$titulo}} </div>
+
+    {{$slot}}
+    {{--  recebendo do component pai 'pagina'
+    tudo que e declarado no strong e recebido como '$slot' --}}
 </div>
 
 <div class="container d-block">
     <form>
         <div class="form-group">
             <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                   placeholder="Enter email">
             <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
         <div class="form-group">
--- views/resources/views/pagina.blade.php
+++ views/resources/views/pagina.blade.php
@@ -11,11 +11,23 @@
             <h3 style="font-size: 30px; font-weight: bold">Componente</h3>
         </div>
 
-        @component('components.meucomponente')
+        @component('components.meucomponente', ['tipo'=>'danger',  'titulo' => 'Erro fatal'])
+            {{--  Second form de passar um dado pro  filho    --}}
+
+
             <strong>Erro: </strong> Sua mensagem de erro.
+
+{{--        @slot('titulo')                                  --}}
+{{--          Erro fatal                                     --}}
+{{--        @endslot                                         --}}
+
+{{--        @slot('tipo')                                    --}}
+{{--            primary                                      --}}
+{{--        @endslot                                         --}}
+            {{--  First form de passar um dado pro  filho    --}}
+
         @endcomponent
         {{-- Passando para o componente atraves do strong --}}
-
 
         {{--<script src="{{ asset('js/app.js')}}" type="text/javascript"></script>--}}
         <script src="{{ URL::to('js/app.js')}}" type="text/javascript"></script>
