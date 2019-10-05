fad851c495197058640638f51cc23fdbb9c147fc
Deyveson Willian <deyvesonn@outlook.com>
2019-10-05 07:48:28 -0400

    Views - Passando parâmetros pro componente Final

--- views/resources/views/pagina.blade.php
+++ views/resources/views/pagina.blade.php
@@ -1,9 +1,5 @@
 <html>
     <head>
-        {{--    importando normal    --}}
-        {{--<link href="{{ asset('css/app.css')}}" rel="stylesheet">--}}
-
-        {{--    URL TO    --}}
         <link href="{{ URL::to('css/app.css')}}" rel="stylesheet">
     </head>
     <body>
@@ -11,25 +7,14 @@
             <h3 style="font-size: 30px; font-weight: bold">Componente</h3>
         </div>
 
-        @component('components.meucomponente', ['tipo'=>'danger',  'titulo' => 'Erro fatal'])
-            {{--  Second form de passar um dado pro  filho    --}}
-
-
-            <strong>Erro: </strong> Sua mensagem de erro.
-
-{{--        @slot('titulo')                                  --}}
-{{--          Erro fatal                                     --}}
-{{--        @endslot                                         --}}
-
-{{--        @slot('tipo')                                    --}}
-{{--            primary                                      --}}
-{{--        @endslot                                         --}}
-            {{--  First form de passar um dado pro  filho    --}}
-
+        @component('components.meucomponente', ['tipo'=>'danger',
+        'titulo' => 'Erro fatal'])
         @endcomponent
-        {{-- Passando para o componente atraves do strong --}}
 
-        {{--<script src="{{ asset('js/app.js')}}" type="text/javascript"></script>--}}
+        @component('components.meucomponente', ['tipo'=>'primary',
+        'titulo' => 'Erro 404'])
+        @endcomponent
+
         <script src="{{ URL::to('js/app.js')}}" type="text/javascript"></script>
     </body>
 </html>
