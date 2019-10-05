202bc1b6941c7fac450f3880a05ba734a93a978e
Deyveson Willian <deyvesonn@outlook.com>
2019-10-05 09:37:15 -0400

    Views - 9. Comparação em Views

---
+++ views/app/Http/Controllers/ProdutoControlador.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProdutoControlador extends Controller
+{
+    public function listar(){
+        $produtoss = [
+            "Notebook Asus i716 GB",
+            "Mouse e Teclado Microsoft USB",
+            "Monitor 21 - Sansung",
+            "Impressora HP",
+            "Disco SSD 512 GB"
+        ];
+
+        $produtos = [];
+
+        return view('produtos', compact('produtos'));
+    }
+}
